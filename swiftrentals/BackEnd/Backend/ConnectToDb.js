a45ed59edac575f6a79bcb5433af55bdfc0a2764
Mjawad12 <workingofficial156@gmail.com>
2023-09-20 02:29:06 -0700

    update

--- swiftrentals/BackEnd/Backend/ConnectToDb.js
+++ swiftrentals/BackEnd/Backend/ConnectToDb.js
@@ -1,5 +1,5 @@
 const db = require("mongoose");
-
+const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 const connectToDb = () => {
--- swiftrentals/BackEnd/Backend/MiddleWare/fetchuser.js
+++ swiftrentals/BackEnd/Backend/MiddleWare/fetchuser.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const path = require("path");
-
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const fetchuser = async (req, res, next) => {
   try {
